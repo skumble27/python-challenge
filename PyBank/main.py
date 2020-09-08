12c5eb2e8e5affdd0cc4cca8a3fb8ad265f1006f
SKumble <skumble27@gmail.com>
2020-09-08 22:01:18 +1000

    Updated Files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,77 +1,59 @@
+import os
 import csv
 import statistics
-import os
 
-budget_file = ('E:/My Documents/Professional Development/Monash University Data Analytics Boot Camp/GitHubRepo/python-challenge/PyBank/Resources/budget_data.csv')
-
-outputfile = open('E:\My Documents\Professional Development\Monash University Data Analytics Boot Camp\GitHubRepo\python-challenge\PyBank\Analysis\Analysis_PyBank.txt',"w")
-
-##budget_file = os.path.join("Resources", "budget_data.csv")
+budget_file = os.path.join("Resources", "budget_data.csv")
+output_budget_file = os.path.join("Analysis", 'Analysis_PyBank.txt', 'w')
 
 months = []
+months_list_indexing = []
 profit_loss = []
-profit_loss_diff = []
-month_list = []
 total_months = 0
-net_total = 0
-
+profit_loss_diff_list = []
+total_profit_loss = 0
 
 with open(budget_file) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
+    csvreader = csv.reader(csvfile, delimiter =',')
+
     csvheader = next(csvreader)
 
     for row in csvreader:
-        total_months += 1
-        net_total += int(row[1])
         months.append(row[0])
+        total_profit_loss += int(row[1])
+        total_months += 1
         profit_loss.append(row[1])
+        
+
+    ## This new list will be required when we want to find the month        that had the highest and lowest profit-loss change
     
     for month in range(1,len(months)):
-        month_list.append(months[month])
-        
-        
-    
-    for i, (cur, nxt) in enumerate(zip(profit_loss, profit_loss[1:])):
-        changes = float(nxt) - float(cur)
-        profit_loss_diff.append(changes)
-    
-    
-## Calculating the Average
-average_change = round(statistics.mean(profit_loss_diff), 2)               
-## Calculating the maximum increase
-max_increase = round(int(max(profit_loss_diff)),0)
+        months_list_indexing.append(months[month])
 
-## Calculating the maximum decrease
-max_decrease = round(int(min(profit_loss_diff)),0)
+    ## In this code, the difference of values from the current and          previous months will be calcuated and appended to a new list
 
-## Referring to the month where max increase occurred
-highest_month = profit_loss_diff.index(max_increase)
-top_month = month_list[highest_month]
+    for i, (pastmnth, presmnth) in enumerate(zip(profit_loss, profit_loss[1:])):
+        changes = float(presmnth) - float(pastmnth)
+        profit_loss_diff_list.append(changes)
 
+## Calculating the average change in profit and loss
+average_change = statistics.mean(profit_loss_diff_list)
 
-## Referring to the month where max decrease occurred
-lowest_month = profit_loss_diff.index(max_decrease)
-bottom_month = month_list[lowest_month]
+maximum_increase_profit = max(profit_loss_diff_list)
+maximum_decrease_profit = min(profit_loss_diff_list)
+
+month_max = profit_loss_diff_list.index(maximum_increase_profit)
+month_max = months_list_indexing[month_max]
+
+month_min = profit_loss_diff_list.index(maximum_decrease_profit)
+month_min = months_list_indexing[month_min]
 
 
-## Providing an output on the Terminal
-
+## Providing an output to the terminal
 print(f' Financial Analysis')
 print(f'-------------------------------------')
 print(f'Total Months: {total_months}')
-print(f'Total: ${net_total}')
-print(f'Average Change: ${average_change}')
-print(f'Greatest Increase in Profits: {top_month} (${max_increase})')
-print(f'Greatest Increase in Profits: {bottom_month} (${max_decrease})')
-
-## Writing the outcome of the Code into Text
-
-
-outputfile.write(f' Financial Analysis\n')
-outputfile.write(f'-------------------------------------\n')
-outputfile.write(f'Total Months: {total_months}\n')
-outputfile.write(f'Total: ${net_total}\n')
-outputfile.write(f'Average Change: ${average_change}\n')
-outputfile.write(f'Greatest Increase in Profits: {top_month} (${max_increase})\n')
-outputfile.write(f'Greatest Increase in Profits: {bottom_month} (${max_decrease})\n')
-outputfile.close()
+print(f'Total: ${total_profit_loss}')
+print(f'Average Change: ${round(average_change,2)}')
+print(f'Greatest Increase in Profits: {month_max} (${maximum_increase_profit})')
+print(f'Greatest Increase in Profits: {month_min} (${maximum_decrease_profit})')
+   
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,56 +1,65 @@
-import statistics
+import os
 import csv
+import statistics
+from collections import Counter
 
-
-poll_file = ('E:/My Documents/Professional Development/Monash University Data Analytics Boot Camp/GitHubRepo/python-challenge/PyPoll/Resources/election_data.csv')
-candidate_list = []
+pypollfile = os.path.join("Resources","election_data.csv")
+outputfile = os.path.join("Analysis", "Analysis_PyPoll.txt")
 
 total_votes = 0
+candidate_tally_raw = []
+candidate_list_count = []
 
-with open(poll_file) as csvfile:
+with open(pypollfile) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-    csvreader = csv.reader(csvfile, delimiter = ',')
     csvheader = next(csvreader)
 
     for row in csvreader:
-        total_votes +=1
-        candidate_list.append(row[2])
+        total_votes += 1
+        candidate_tally_raw.append(row[2])
+## You could use a dictionary to analyse
+
+## Dictionary helps to loop through the keys
+candidate_list_count = dict(Counter(candidate_tally_raw)) 
+
+##Will give the value back for the key
+top_rank = candidate_list_count[list(candidate_list_count.keys())[0]]
+second_rank = candidate_list_count[list(candidate_list_count.keys())[1]]
+third_rank = candidate_list_count[list(candidate_list_count.keys())[2]]
+fourth_rank = candidate_list_count[list(candidate_list_count.keys())[3]]
+
+## Providing the specific name back
+top_rank_name = list(candidate_list_count.keys())[0] 
+second_rank_name = list(candidate_list_count.keys())[1]
+third_rank_name = list(candidate_list_count.keys())[2]
+fourth_rank_name = list(candidate_list_count.keys())[3]
+
+## Writing the file into  a text
+with open(outputfile, 'w') as datafile:
+    datafile.write(f'Election Results\n')
+    datafile.write(f'---------------------------------\n')
+    datafile.write(f'Total Votes: {total_votes}\n')
+    datafile.write(f'{top_rank_name}: {round(100*(top_rank/total_votes),3)}% ({top_rank})\n')
+    datafile.write(f'{second_rank_name}: {round(100*(second_rank/total_votes),3)}% ({second_rank})\n')
+    datafile.write(f'{third_rank_name}: {round(100*(third_rank/total_votes),3)}% ({third_rank})\n')
+    datafile.write(f'{fourth_rank_name}: {round(100*(fourth_rank/total_votes),3)}% ({fourth_rank})\n')
+    datafile.write(f'---------------------------------\n')
+    datafile.write(f'Winner: {top_rank_name}\n')
+    datafile.write(f'---------------------------------\n')
+    datafile.close()
+
+ 
+## Displaying Data on the Terminal
+    print(f'Election Results')
+    print(f'---------------------------------')
+    print(f'Total Votes: {total_votes}')
+    print(f'{top_rank_name}: {round(100*(top_rank/total_votes),3)}% ({top_rank})')
+    print(f'{second_rank_name}: {round(100*(second_rank/total_votes),3)}% ({second_rank})')
+    print(f'{third_rank_name}: {round(100*(third_rank/total_votes),3)}% ({third_rank})')
+    print(f'{fourth_rank_name}: {round(100*(fourth_rank/total_votes),3)}% ({fourth_rank})')
+    print(f'---------------------------------')
+    print(f'Winner {top_rank_name}')
+    print(f'---------------------------------')
+
     
-unique_candidates = set(candidate_list)
-otooley_vote_count = float(candidate_list.count('O\'Tooley'))
-correy_vote_count = float(candidate_list.count('Correy'))
-khan_vote_count = float(candidate_list.count('Khan'))
-li_vote_count = float(candidate_list.count('Li'))
-unique_candidates = list(unique_candidates)
-
-## Calculating the percentages of votes for each candidate
-otooley_percentage_vote = round(float(otooley_vote_count)/float(total_votes) * float(100),3)
-
-correy_percentage_vote = round(float(correy_vote_count)/float(total_votes) * float(100),3)
-
-khan_percentage_vote = round(float(khan_vote_count)/float(total_votes) * float(100),3)
-
-li_percentage_vote = round(float(li_vote_count)/float(total_votes) * float(100),3)
-
-## Creating a Dictionary of Candidates
-count_list = [otooley_vote_count, correy_vote_count, khan_vote_count, li_vote_count]
-
-percentage_list = [otooley_percentage_vote, correy_percentage_vote, khan_percentage_vote, li_percentage_vote]
-
-
-## Creating a nested dictionary
-Summary_Candidates = dict({
-1: {'Name': [unique_candidates[0]], 'Vote': [count_list[0]], 'Percentage': [percentage_list[0]]},
-
-2:{'Name': [unique_candidates[1]], 'Vote': [count_list[1]], 'Percentage': [percentage_list[1]]},
-
-3:{'Name': [unique_candidates[2]], 'Vote': [count_list[2]], 'Percentage': [percentage_list[2]]},
-
-4:{'Name': [unique_candidates[3]], 'Vote': [count_list[3]], 'Percentage': [percentage_list[3]]}
-
-})
-
-
-
-
-
