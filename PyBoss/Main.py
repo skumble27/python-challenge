f988c37103d6ea2881e975d93188ef2ca66cb065
SKumble <skumble27@gmail.com>
2020-09-11 20:00:04 +1000

    PyBos Addition

--- PyBoss/Main.py
+++ PyBoss/Main.py
@@ -1,4 +1,19 @@
+## Importing the relevant modules
 import os
 import csv
+import us_state_abbrev as usabr
 
-employeedata = os.path.join()
+## Changing the terminal directory to the location to the location of the main python script
+os.chdir(os.path.dirname(__file__))
+
+## Creating the file paths
+py_boss_file = os.path.join("Resources", "employee_data.csv")
+
+py_boss_output_file = os.path.join("Cleaned_file","cleaned_employee_data.csv")
+
+## Creating empty lists in which to store relevant information
+emp_ID_list = []
+emp_name_list = []
+DOB_list = []
+SSN_list = []
+state_list = []
